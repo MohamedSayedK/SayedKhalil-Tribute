984fc84180d72de4b45049476734aa622441b8d2
MohamedSayedK <mohamedsk18@gmail.com>
2021-03-03 23:10:58 +0200

    Replaced A tags in list with buttons

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ $(document).ready(function(){
 
 document.addEventListener('DOMContentLoaded',init);
 
+
+const openArtistListButtons = document.querySelectorAll('[data-model-target]')
+
+const openArtistListButtons = document.querySelectorAll('[data-close-button]')
+
+const overlay = document.getElementById('overlay')
+
+openArtistListButtons.forEach(a => {
+a.addEventListener('click',() => {
+    const authorsList = document.querySelector(a.dataset.modelTarget)
+    openArtistListButtons()
+    })
+})
