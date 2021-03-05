4d85aed3a5cd2a0f9f3c87f584c7913619451f87
MohamedSayedK <mohamedsk18@gmail.com>
2021-03-05 23:41:18 +0200

    got the pop up to work but still needs fixing

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ document.addEventListener('DOMContentLoaded',init);
 
 
 function toggle(){
-    var blur = document.getElementById('blur');
-    blur.classList.toggle('active')
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active')
 }
