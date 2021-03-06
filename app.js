b460bc25493688d04ed7f022518f5e69f350bb7d
MohamedSayedK <mohamedsk18@gmail.com>
2021-03-06 23:18:33 +0200

    added pop ups

--- app.js
+++ app.js
@@ -9,3 +9,8 @@ function toggle(){
     var popup = document.getElementById('popup');
     popup.classList.toggle('active')
 }
+
+function toggle2(){
+    var popup = document.getElementById('popup2');
+    popup.classList.toggle('active')
+}
